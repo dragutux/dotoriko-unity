0c0f7903b691b1c4ff43e3a8dfd5db50efbb3ddf
NoxCaos <glebx95@gmail.com>
2016-02-15 00:36:40 +0200

    Added base for menu ui manager

---
+++ UI/Menu/MenuManager.cs
@@ -0,0 +1,12 @@
+///
+/// DotOriko v1.0
+/// Menu manager
+/// By NoxCaos 14.02.2016
+///
+using DotOriko.Core; 
+
+namespace DotOriko.UI.Menu {
+    public abstract class MenuManager : DotOrikoSingleton<MenuManager> {
+
+    }
+}
