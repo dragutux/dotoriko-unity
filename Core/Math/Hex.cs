05d448ae791c392b93827fc36ae755992703d1cc
NoxCaos <glebx95@gmail.com>
2016-02-14 11:52:41 +0200

    Added some math utilities

---
+++ Core/Math/Hex.cs
@@ -0,0 +1,23 @@
+﻿///
+/// DotOriko v1.0
+/// Math extentions
+/// By NoxCaos 11.02.2016
+/// 
+using UnityEngine;
+using System.Globalization;
+
+namespace DotOriko.Math {
+    public static class Hex {
+        public static string ColorToHex(Color32 color) {
+            string hex = color.r.ToString("X2") + color.g.ToString("X2") + color.b.ToString("X2");
+            return hex;
+        }
+
+        public static Color HexToColor(string hex) {
+            byte r = byte.Parse(hex.Substring(0, 2), NumberStyles.HexNumber);
+            byte g = byte.Parse(hex.Substring(2, 2), NumberStyles.HexNumber);
+            byte b = byte.Parse(hex.Substring(4, 2), NumberStyles.HexNumber);
+            return new Color32(r, g, b, 255);
+        }
+    }
+}
