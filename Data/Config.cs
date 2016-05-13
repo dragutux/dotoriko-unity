a89de24d66fb6b972f31b3bcd3bef276ff8c7094
KsYay <nyaka.toor@gmail.com>
2016-05-14 00:47:53 +0300

    Added new basics of serializing

--- Data/Config.cs
+++ Data/Config.cs
@@ -1,218 +1,91 @@
-// 
-//  Config.cs
-//  
-//  Author:
-//       Denis Oleynik <denis@meliorgames.com>
-// 
-//  2013 Melior Games Inc.
-// 
-//  
+﻿using UnityEngine;
+using Newtonsoft.Json;
+using DotOriko.Core;
 using System;
-using UnityEngine;
 using System.IO;
-using DotOriko.Data.Serialization;
 
-namespace DotOriko.Data
-{
-	public enum StorageType
-	{
-		Documents,
-		StreamingAssets
-	}
-	
-	
-	public abstract class Config<TConfig,TSerializationPolicy> 
-		where TSerializationPolicy:SerializationPolicy,new()
-			where TConfig:Config<TConfig,TSerializationPolicy>
-	{
-		public const string CONFIGS_FOLDER = "Configs";
-		#if UNITY_EDITOR
-		private static readonly string _applicationDataPath = Application.dataPath;
-		#endif
-		private static string _documentsPath;
-		
-		private string _name;
-		
-		private string _path;
-		
-		public Config (String name)
-		{
-			_name = GetNameWithExtension(name);
-		}
-		
-		public virtual void OnRestored()
-		{
-		}
+namespace DotOriko.Data {
 
-		public static Stream GenerateStreamFromString(string s)
-		{
-			MemoryStream stream = new MemoryStream();
-			StreamWriter writer = new StreamWriter(stream);
-			writer.Write(s);
-			writer.Flush();
-			stream.Position = 0;
-			return stream;
-		}
-		
-		public static TConfig Load(StorageType storage, string name) 
-		{
-			return Load(Path.Combine(GetPath(storage), GetNameWithExtension(name)));
-		}
+    public enum StorageType {
+        Documents, Resources
+    }
 
-		public static TConfig Load(TextAsset asset){
-			TSerializationPolicy serializer = new TSerializationPolicy();
-			TConfig config = null;
+    public abstract class Config<TConfig> where TConfig : Config<TConfig> {
 
-			using(Stream stream = GenerateStreamFromString(asset.text))
-			{
-				config = serializer.Restore<TConfig>(stream);
-			}
+        public const string CONFIGS_FOLDER = "Configs/";
+        public const string EXTENTION = ".json";
 
-			config._path = asset.name;
-			config.OnRestored();
+        private static string _documentsPath;
 
-			return config;
-		}
-		
-		private static TConfig Load(StorageType storage, string name, MemoryStream stream) 
-		{
-			TSerializationPolicy serializer = new TSerializationPolicy();
-			
-			TConfig config = null;
-			
-			using(stream)
-			{
-				config = serializer.Restore<TConfig>(stream);
-			}
-			
-			config._path = Path.Combine(GetPath(storage), GetNameWithExtension(name));
-			config.OnRestored();
-			
-			return config;
-		}
-		
-		private static TConfig Load(string filepath) 
-		{
-            TSerializationPolicy serializer = new TSerializationPolicy();
-			
-			TConfig config = null;
-			
-			using(Stream stream = File.OpenRead(filepath))
-			{
-				config = serializer.Restore<TConfig>(stream);
-			}
-			
-			config._path = filepath;
-			config.OnRestored();
-			
-			return config;
-		}
-		
-		private static string GetNameWithExtension(string name)
-		{
-			TSerializationPolicy serializer = new TSerializationPolicy();
-			return string.Format("{0}.{1}",name, serializer.FileExtention);
-		}
-		
-		public static bool IsExists (StorageType storage, string name)
-		{
-			return File.Exists(Path.Combine(GetPath(storage), GetNameWithExtension(name)));
-		}
-		
-		public static string GetPath(StorageType storage)
-		{
-			if (storage == StorageType.StreamingAssets)
-			{
-				return GetDocumentsPath();
-			}
-			
-			string folder = string.Empty;
-			
-			if(storage == StorageType.Documents)
-				folder = GetDocumentsPath();
-			else
-			{
-				folder = Application.streamingAssetsPath;
-			}
-			
-			return Path.Combine(folder,CONFIGS_FOLDER);
-		}
-		
-		public static string GetDocumentsPath()
-		{
-			if (!string.IsNullOrEmpty(_documentsPath))
-				return _documentsPath;
-			
-			#if UNITY_EDITOR
-			_documentsPath = System.IO.Path.Combine(_applicationDataPath,"..");
-			_documentsPath = System.IO.Path.Combine(_documentsPath,"Data");
-			#else
-			_documentsPath = Application.persistentDataPath;
-			#endif
-			
-			return _documentsPath;
-		}
-		
-		public void Save(StorageType storage)
-		{
-			if(string.IsNullOrEmpty(_path))
-			{
-				#if UNITY_EDITOR
-				_path = GetPath( storage );
-				#else
-				_path = GetPath( storage );
-				#endif
-				
-				if(!Directory.Exists(_path))
-					Directory.CreateDirectory(_path);
-				
-				_path = Path.Combine(_path,_name);
-			}
-			
-			TSerializationPolicy serializer = new TSerializationPolicy();
-			
-			using(Stream stream = File.Open(_path, FileMode.Create))
-			{
-				serializer.Store<TConfig>((TConfig)this,stream);
-			}
-		}
-		
-		public void Save(StorageType storage, string filename)
-		{
-			string filenameWithExt = GetNameWithExtension(filename);
-			#if UNITY_EDITOR
-			_path = (storage == StorageType.Documents)? 
-				GetPath( storage ) : CombinePath(_applicationDataPath, "Editor", "packedDocuments", "Configs");
-			_path = Path.Combine(_path,filenameWithExt);
-			#else
-			if(string.IsNullOrEmpty(_path))
-			{
-				_path = GetPath( storage );
-				
-				if(!Directory.Exists(_path))
-					Directory.CreateDirectory(_path);
-				
-				_path = Path.Combine(_path,filenameWithExt);
-			}
-			#endif
-			
-			TSerializationPolicy serializer = new TSerializationPolicy();
-			
-			using(Stream stream = File.Open(_path, FileMode.Create))
-			{
-				serializer.Store<TConfig>((TConfig)this,stream);
-			}
-		}
+        private string _name;
 
-		public static string CombinePath(params string[] names)
-		{
-			string result = names[0];
-			
-			for (int i = 1; i < names.Length; i++)
-			{
-				result = Path.Combine(result, names[i]);
-			}
-			return result;
-		}
-	}
+        public Config(string name) {
+            _name = CONFIGS_FOLDER + name + EXTENTION;
+        }
+
+        public virtual void OnRestored() { }
+
+        public static TConfig Load(StorageType type, string name) {
+            if (type == StorageType.Resources) {
+                TextAsset tAsset = null;
+                try {
+                    tAsset = Resources.Load(CONFIGS_FOLDER + name + EXTENTION) as TextAsset;
+                } catch (Exception e) {
+                    Errors.Log("[DotOriko.Data Config] EXCEPTION: {1}", e.Message);
+                }
+                return Load(tAsset);
+            } else if (type == StorageType.Documents) {
+                TConfig config = null;
+                try {
+                    var data = File.ReadAllText(Application.persistentDataPath + name);
+                    config = JsonConvert.DeserializeObject<TConfig>(data);
+                    config.OnRestored();
+                } catch (IOException e) {
+                    Errors.Log("[DotOriko.Data Config] EXCEPTION: {1}", e.Message);
+                }
+                return config;
+            }
+            return null;
+        }
+
+        public static TConfig Load(TextAsset asset) {
+            TConfig config = null;
+
+            try {
+                config = JsonConvert.DeserializeObject<TConfig>(asset.text);
+                config.OnRestored();
+            } catch (Exception e) {
+                Errors.Log("[DotOriko.Data Config] EXCEPTION: {1}", e.Message);
+            }
+
+            return config;
+        }
+
+        public static string GetDocumentsPath() {
+            if (!string.IsNullOrEmpty(_documentsPath))
+                return _documentsPath;
+
+#if UNITY_EDITOR
+            _documentsPath = Path.Combine(Application.dataPath, "..");
+            _documentsPath = Path.Combine(_documentsPath, "Data/");
+            Directory.CreateDirectory(_documentsPath);
+#else
+		_documentsPath = Application.persistentDataPath;
+#endif
+
+            return _documentsPath;
+        }
+
+        public string GetJson() {
+            return JsonConvert.SerializeObject(this);
+        }
+
+        public void Save(StorageType storage) {
+            try {
+                File.WriteAllText(GetDocumentsPath() + _name + EXTENTION, GetJson());
+            } catch (IOException e) {
+                Errors.Log("[DotOriko.Data Config] EXCEPTION: {1}", e.Message);
+            }
+        }
+    }
 }
