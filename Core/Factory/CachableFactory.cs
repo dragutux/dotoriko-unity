9df55bfd8ed8a06ec68ca4cac43273c0e28169fb
NoxCaos <glebx95@gmail.com>
2016-05-14 15:27:45 +0300

    Change factorytype to enum

--- Core/Factory/CachableFactory.cs
+++ Core/Factory/CachableFactory.cs
@@ -6,30 +6,15 @@
 
 using System.Collections.Generic;
 using UnityEngine;
-using System.Collections.ObjectModel;
+using System;
 
 namespace DotOriko.Core.Factory {
-    public abstract class FactoryType : EnumBaseType<FactoryType> {
 
-        public FactoryType(int key, string value) : base(key, value) { }
+    public abstract class CachableFactory<T, N> 
+        where T : UnityEngine.Object
+        where N : struct, IConvertible {
 
-        public static ReadOnlyCollection<FactoryType> GetValues() {
-            return GetBaseValues();
-        }
-
-        public static FactoryType GetByKey(int key) {
-            return GetBaseByKey(key);
-        }
-
-        public static FactoryType FromString(string val) {
-            return BaseFromString(val);
-        }
-
-    }
-
-    public abstract class CachableFactory<T> where T : Object {
-
-        protected static Dictionary<FactoryType, string> typeToUrl;
+        protected static Dictionary<N, string> typeToUrl;
 
         private static Dictionary<string, T> cachedItems;
 
@@ -37,11 +22,11 @@ namespace DotOriko.Core.Factory {
             return GetItemByLink(string.Format("{0}/{1}", typeof(T), name));
         }
 
-        public static T GetItem(FactoryType type) {
+        public static T GetItem(N type) {
             return GetItemByLink(typeToUrl[type]);
         }
 
-        public static B SpawnItem<B>(FactoryType type) where B : MonoBehaviour {
+        public static B SpawnItem<B>(N type) where B : MonoBehaviour {
             return GameObject.Instantiate(GetItem(type)) as B;
         }
 
