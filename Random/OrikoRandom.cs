30944c8ea51e9c4f6b608dc313a3c8d29a50f923
NoxCaos <glebx95@gmail.com>
2016-05-17 18:07:22 +0300

    Added random namespace

---
+++ Random/OrikoRandom.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace DotOriko.Random {
+
+	/// <summary>
+	/// DotOriko random extentions and shortcut functions
+	/// Based on UnityEngine.Random
+	/// </summary>
+	public static class OrikoRandom {
+
+		/// <summary>
+		/// Provides a probability in a form of boolen.
+		/// Looks like 1:prob
+		/// </summary>
+		/// <param name="prob">The probability</param>
+		public static bool Probability(int prob) {
+			 return UnityEngine.Random.Range (0, prob) == 0;
+		}
+	}
+}
